846659ac13e99b177c77a6b6959720eb4d42e67a
ET Ultra cyberman <timofeyefimenko.07@mail.ru>
2026-04-08 21:32:47 +0700

    updated brain-even and fixed engine

--- src/games/even.js
+++ src/games/even.js
@@ -1,22 +1,22 @@
-import { runGame } from "../engine.js"
+import { runGame } from '../engine.js'
 
 // функция принимает на вход то число, которое делится на два без остатка, тоесть четное.
 function isEven(number) {
-    return number % 2 === 0
+  return number % 2 === 0
 }
 
 // функция возвращает случайную генерацию
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function getRoundData() {
-    const randomNumber = getRandomNumber(1, 100)
-    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no'
-    const question = String(randomNumber)
-    return { question, correctAnswer }
+  const randomNumber = getRandomNumber(1, 100)
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no'
+  const question = String(randomNumber)
+  return { question, correctAnswer }
 }
 
 export default function playBrainEven() {
-    runGame('Answer "yes" if the number is even, otherwise answer "no".', getRoundData)
+  runGame('Answer "yes" if the number is even, otherwise answer "no".', getRoundData)
 }
